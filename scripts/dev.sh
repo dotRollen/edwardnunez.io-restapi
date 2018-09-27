570d20a4f3377f7dc1029d46825e8833a759aaeb
dotRollen <edwardnnz@gmail.com>
2018-09-27 17:36:50 -0400

    rename build folder for python

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Download requirements to build cache
-env/bin/pip download -d build/ -r requirements/dev.txt --no-input
+env/bin/pip download -d build-py/ -r requirements/dev.txt --no-input
 
 # Install application test requirements
-env/bin/pip  install --no-index -f build/ -r requirements/dev.txt
+env/bin/pip  install --no-index -f build-py/ -r requirements/dev.txt
 
 # Run test.sh arguments
 exec $@
